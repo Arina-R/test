9cdb11ec1da9f5dd920b5c131609d80a4870b96c
Arina-R <remizova.2002@list.ru>
2021-03-30 02:59:21 +0300

    Signed-off-by: Arina-R <remizova.2002@list.ru>

--- тест/Form1.cs
+++ тест/Form1.cs
@@ -16,5 +16,20 @@ namespace тест
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
